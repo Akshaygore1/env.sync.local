f33576b650882f92b0575b6ec7412bb70c3b6142
Arnav Gupta <dev@championswimmer.in>
2026-02-09 02:52:02 +0000

    v2.0.3
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

--- src/internal/config/config.go
+++ src/internal/config/config.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	Version              = "2.0.2"
+	Version              = "2.0.3"
 	DefaultPort          = "5739"
 	Service              = "_envsync._tcp"
 	DefaultInitTimestamp = "1970-01-01T00:00:00Z"
